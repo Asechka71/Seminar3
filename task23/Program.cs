8af5f9bf2a3b119ab9821186ff7fe8a37f5581df
Anastasiia <kariakina.nastia@example.com>
2022-08-18 15:09:11 +0300

    add Task 23

---
+++ task23/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Задача 23
+Напишите программу, которая принимает на вход число (N) 
+и выдаёт таблицу кубов чисел от 1 до N.
+*/
+Console.WriteLine("Введите число: ");
+int length = Convert.ToInt32(Console.ReadLine());
+void CubeArray(int [] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+      collection [index] = (index + 1)*(index + 1)*(index + 1);
+      index ++;
+    }
+Console.WriteLine(String.Join(" ", collection));
+}
+int arraySize = length;
+int [] array = new int[arraySize];
+CubeArray(array);
